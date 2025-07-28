927546dc249bb3e75123812469d00d2feb85defe
Daniel Newbrook <dwn1c21@soton.ac.uk>
2025-07-28 12:56:14 +0100

    Update neon test to include div and mult

--- software/src/neon_tests/neon_tests.c
+++ software/src/neon_tests/neon_tests.c
@@ -1,32 +1,48 @@
 #include "uart_stdout.h"
 #include <stdio.h>
 #include "arm_neon.h"
+#include "system.h"
 
 int main(void) {
     uint32_t errors = 0;
+    float32x4_t v1 = { 1.0, 2.0, 3.0, 4.0 }, v2 = { 4.0, 3.0, 2.0, 1.0 };
+
+    float32x4_t sum;
+    float32x4_t div;
+    float32x4_t mul;
+
     UartStdOutInit();
 
     printf("Neon Tests - SoCLabs MegaSoC\n");
 
-    float f1 = 2.200002;
-    float f2 = 2.200001;
-    float ans = 1.0;
-    printf("Starting SP Floating point ...\n");
-    for (int i=0; i<10;i++){
-        ans *= f1;
-        ans *= f2;
+    printf("Neon Sum\n");
+    sum = vaddq_f32(v1, v2);
+    for (int i=0; i<4; i++){
+        if(sum[i] != 5.0){errors++;}
     }
-    printf("...Finshed\n");
-    printf("ans = %f\n",ans);
+    if(errors!=0){TEST_FAIL();}
 
-    printf("Starting SP Vector Floating point ...\n");
+    printf("Neon Div\n");
+    div = vdivq_f32(v1,v2);
+    if(div[0]!=0.25){errors++;}
+    if(div[2]!=1.5){errors++;}
+    if(div[3]!=4.0){errors++;}
+    if(errors!=0){
+        printf("div[0] = %f\n", div[0]);
+        printf("div[1] = %f\n", div[1]);
+        printf("div[2] = %f\n", div[2]);
+        printf("div[3] = %f\n", div[3]);
+        TEST_FAIL();
+    }
 
-    float32x4_t v1 = { 1.0, 2.0, 3.0, 4.0 }, v2 = { 1.0, 1.0, 1.0, 1.0 };
-    float32x4_t sum = vaddq_f32(v1, v2);
-    printf("sum = %f\n",sum[0]);
+    printf("Neon Multiply\n");
+    mul = vmulq_f32(v1,v2);
+    if(mul[0]!=4.0){errors++;}
+    if(mul[1]!=6.0){errors++;}
+    if(mul[2]!=6.0){errors++;}
+    if(mul[3]!=4.0){errors++;}
 
-    printf("...Finshed\n");
-
-    UartEndSimulation();
+    if(errors!=0){TEST_FAIL();}
+    else{TEST_PASS();}
 }
 
